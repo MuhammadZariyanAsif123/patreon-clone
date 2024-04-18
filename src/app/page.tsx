97df40dfe1f5569f1799e305266b369c371ab337
MuhammadZariyanAsif123 <muhammadzariyan@gmail.com>
2024-04-18 18:55:07 +0500

    Masked Login/SignUp UI

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import LoginSignUp from "./components/Login-SignUp";
+
+export default function Home() {
+  return (
+    <>
+     <div>
+      <div>
+        <LoginSignUp/>
+      </div>
+     </div>
+    </>  
+  );
+}
